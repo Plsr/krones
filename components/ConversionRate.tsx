67b31f029023a5560e20ef3bebc9c9d80704351a
Plsr <christian@gigs.com>
2024-07-07 08:41:11 +0200

    Add more prices

--- components/ConversionRate.tsx
+++ components/ConversionRate.tsx
@@ -1,26 +1,18 @@
-import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
-import { Coins } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
+import { Coins } from 'lucide-react'
 
-const rate = 0.13;
+const rate = 0.13
 
 export const ConversionRate = () => {
   return (
     <Card>
-      <CardHeader>
-        <CardTitle>
-          <div className="flex items-center gap-4">
-            <Coins className="h-4 w-4" />
-            <span>Conversion Rate</span>
-          </div>
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
+      <CardContent className='pt-6'>
         <div>1 DKK = {rate} €</div>
-        <div className="text-sm">Roughly {Math.ceil(1 / 0.13)} kr. are 1 €</div>
-        <div className="text-xs dark:text-neutral-500 text-neutral-400 mt-2">
+        <div className='text-sm'>Roughly {Math.ceil(1 / 0.13)} kr. are 1 €</div>
+        <div className='text-xs dark:text-neutral-500 text-neutral-400 mt-2'>
           Last update: Static
         </div>
       </CardContent>
     </Card>
-  );
-};
+  )
+}
--- pages/prices.tsx
+++ pages/prices.tsx
@@ -1,7 +1,60 @@
 import { Screen } from '@/components/Screen'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+const rate = 0.13
+
+const data = [
+  { name: 'Gasoline (1 Ltr.)', price: 15.79 },
+  { name: 'Cappucino', price: 45 },
+  { name: 'Whopper', price: 70 },
+  { name: 'Big Mac', price: 49 },
+  { name: 'Milk', price: 14 },
+  { name: 'Oat Milk', price: 22 },
+]
 
 const Prices = () => {
-  return <Screen>Prices</Screen>
+  return (
+    <Screen>
+      <Table>
+        <TableHeader>
+          <TableRow className='border-b border-b-neutral-200 dark:border-b-neutral-700'>
+            <TableHead>Item</TableHead>
+            <TableHead>DKK</TableHead>
+            <TableHead>EUR</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.map((item) => (
+            <TableItem key={item.name} name={item.name} price={item.price} />
+          ))}
+        </TableBody>
+      </Table>
+    </Screen>
+  )
+}
+
+type TableItemProps = {
+  name: string
+  price: number
+}
+
+const TableItem = ({ name, price }: TableItemProps) => {
+  return (
+    <TableRow className='border-b border-b-neutral-200 dark:border-b-neutral-700'>
+      <TableCell>
+        <span>{name}</span>
+      </TableCell>
+      <TableCell>{price} kr.</TableCell>
+      <TableCell>{(price * rate).toFixed(2)} €</TableCell>
+    </TableRow>
+  )
 }
 
 export default Prices
